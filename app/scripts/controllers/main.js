e4f09d2b90dc917f9e9a35cd508478c28bee6743
Aimar Rodriguez <aimar.rodriguez.s@gmail.com>
2014-04-04 15:13:42 +0200

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('hackForGood2014App')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
