67b7ec3852dea24317d7d9171d9540dbd3713062
Aimar Rodriguez <aimar.rodriguez.s@gmail.com>
2014-04-05 12:12:04 +0200

    Editor finished

--- app/scripts/controllers/routes.js
+++ app/scripts/controllers/routes.js
@@ -2,17 +2,17 @@
 
 angular.module('hackForGood2014App')
   .controller('RoutesCtrl', function ($scope, Route) {
-     $scope.routes = [{
-     	name: 'Ruta de prueba',
-     	description: 'Descripcion muy larga de todo este tema, no me voy a poner a copiar un lorem ipsum ahora.',
-     	id: '1122312425253',
-     	author: {
-     		name: 'Administrador',
-     		email: 'admin@hackforfood.com'
-     	}
-     }];
+     $scope.routes = [];
      Route.query().$promise.then(function sucess(data){
-        console.log(data);
+        for(var key in data)
+            $scope.routes.push({
+                name: data[key].name,
+                description: data[key].description,
+                owner: {
+                    name: data[key].owner.name,
+                    owner: data[key].owner.email
+                }
+            })
      });
 
 
